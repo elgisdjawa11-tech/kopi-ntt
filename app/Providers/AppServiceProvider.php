6a7689138d3f323c43ad73aea3ab6cfd49b969e0
Eulogius Jawa <elgisdjawa11@gmail.com>
2026-04-29 16:46:31 +0700

    fix: force https agar tidak muncul warning secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Tambahkan baris ini agar semua link otomatis jadi HTTPS di Railway
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
