b3d0dd4823c610b1c910db21777112daf7687ce7
Eulogius Jawa <elgisdjawa11@gmail.com>
2026-04-29 15:23:04 +0700

    fix: update seeder menggunakan username dan p

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Kita buat satu user admin secara manual agar tidak bentrok dengan factory lama
+        User::create([
+            'name' => 'Admin Kopi NTT',
+            'username' => 'admin',
+            'phone' => '08123456789',
+            'city' => 'Kupang',
+            'password' => Hash::make('password123'), // Passwordnya: password123
+            'role' => 'admin',
         ]);
     }
 }
